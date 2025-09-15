5079db1249cff0d0a3c6d71551badf42430c23e7
vinibertunho <vinicius.m.silva36@aluno.senai.br>
2025-09-15 10:20:34 -0300

    feat: cria o insert da tabela

--- queries/01-inseet-artist.sql
+++ queries/01-inseet-artist.sql
@@ -10,3 +10,16 @@ MUSICA_MAIS_FAMOSA VARCHAR(50) NOT NULL,
 ATIVO BOOLEAN DEFAULT TRUE
 );
 
+INSERT INTO ARTIST (NAME, TIPO, MEMBROS, GENERO_MUSICAL, NACIONALIDADE, ANO_DE_INICIO, MUSICA_MAIS_FAMOSA, ATIVO) VALUES
+('Racionais', 'grupo de rap', 4, 'rap', 'brasileira', 1988, 'Diário de um detento', TRUE),
+('facção central', 'grupo de rap', 5, 'rap', 'brasileira', 1989, 'Hoje', TRUE),
+('mc hariel', 'cantor solo', NULL, 'funk', 'brasileira', 1996, 'Hoje eu tô um monstro', TRUE),
+('NLE choppa', 'cantor solo', NULL, 'rap', 'americana', 2019, 'Shotta flow', TRUE),
+('Drake', 'cantor solo', NULL, 'rap', 'canadense', 2006, 'God\s plan' TRUE),
+('50 cent', 'cantor solo', NULL, 'rap', 'americana', 1996, 'In da club', FALSE),
+('Eminem', 'cantor solo', NULL, 'rap', 'americana', 1996, 'Lose yourself', TRUE),
+('keyshia cole', 'cantora solo', NULL, 'r&b', 'americana', 2005, 'Love', TRUE),
+('akon', 'cantor solo', NULL, 'r&b', 'americana', 2004, 'lonely', TRUE),
+('chief keef', 'cantor solo', NULL, 'rap', 'americana', 2011, 'I don\t like', TRUE);
+
+SELECT * FROM ARTIST;
