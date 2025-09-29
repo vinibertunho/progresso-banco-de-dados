edfa40c9d29a7480c845a0129917aa813a5a0022
vinibertunho <vinicius.m.silva36@aluno.senai.br>
2025-09-29 11:40:28 -0300

    faz o draw io

---
+++ diagramas/testes/acentos.sql
@@ -0,0 +1,14 @@
+CREATE TABLE testes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+INSERT INTO
+    testes (nome)
+VALUES
+    ('Vinicíus'),
+    ('Márcia'),
+    ('Renée'),
+    ('Bárbara'),
+    ('Cézar'),
+    ('Lúcia');
