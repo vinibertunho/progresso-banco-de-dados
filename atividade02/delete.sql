c13a601a0bc2483ca4bbc088924763005f313ae1
vinibertunho <vinicius.m.silva36@aluno.senai.br>
2025-09-29 10:35:18 -0300

    termine o diagrama

--- atividade02/delete.sql
+++ atividade02/delete.sql
@@ -8,3 +8,7 @@ WHERE preco < 20.00;
 
 DELETE FROM produtos
 WHERE marca = 'Glossier';
+
+
+DELETE FROM produtos
+WHERE estoque BETWEEN 1 AND 5;
--- atividade02/uptade.sql
+++ atividade02/uptade.sql
@@ -1,3 +1,4 @@
+
 UPDATE produtos
 SET preco = preco * 0.8
 WHERE categoria = 'skincare';
@@ -28,3 +29,17 @@ LIMIT 1;
 UPDATE produtos
 SET preco = preco * 0.10
 WHERE estoque > 50;
+
+
+UPDATE produtos
+SET categoria = 'perfumes'
+WHERE id IN (19, 7, 11, 15);
+
+UPDATE produtos
+SET preco = preco + preco * 0.30
+WHERE categoria = 'perfumes';
+
+UPDATE produtos
+SET estoque = estoque + 25
+WHERE preco > 150.00;
+
